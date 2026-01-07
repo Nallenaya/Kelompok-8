f408bc180729e62aa5b2d6fbd9418d001412b242
Nallenaya <nallepoetra2809@gmail.com>
2026-01-07 16:31:47 +0700

    Perbaiki database

--- desa_dawuan.sql
+++ desa_dawuan.sql
@@ -27,10 +27,10 @@ CREATE TABLE `data` (
 --
 
 INSERT INTO `data` (`id`, `title`, `description`, `image`, `created_at`, `updated_at`) VALUES
-(1, '85000', 'Luas wilayah desa sumber kedawung', 'service.png', '2022-12-21 05:04:32', '2022-12-21 05:04:32'),
-(2, '26', 'Jumlah Kartu Keluarga di desa dumber kedawung', 'service.png', '2022-12-21 05:05:27', '2022-12-21 05:05:27'),
-(3, '150', 'Jumla Penduduk di desa Dawuan', 'service.png', '2022-12-21 05:06:00', '2022-12-21 05:06:00'),
-(4, '10', 'Prestasi desa Dawuan', 'service.png', '2022-12-21 05:06:32', '2022-12-21 05:06:32');
+(1, '85000', 'Luas wilayah desa dawuan', 'service.png', '2026-01-03 22:30:02', '2026-01-07 02:15:44'),
+(2, '26', 'Jumlah Kartu Keluarga di desa dawuan', 'service.png', '2026-01-03 22:30:02', '2026-01-07 02:15:53'),
+(3, '150', 'Jumla Penduduk di desa Dawuan', 'service.png', '2026-01-03 22:30:02', '2022-12-21 05:06:00'),
+(4, '10', 'Prestasi desa Dawuan', 'service.png', '2026-01-03 22:30:02', '2022-12-21 05:06:32');
 
 -- --------------------------------------------------------
 
@@ -52,8 +52,8 @@ CREATE TABLE `events` (
 --
 
 INSERT INTO `events` (`id`, `title`, `description`, `image`, `created_at`, `updated_at`) VALUES
-(1, 'Rapat Tahun Baru', 'Tasyakuran bersama warga dawuan untuk mempererat silaturahm', 'galeri d1.jpg', '2022-12-21 05:53:48', '2026-01-06 15:55:53'),
-(2, 'Musyawarah Desa', 'Penyambung keharmonisan desa kedawung dengan menjaga kesehatan', 'galeri d3.jpg', '2022-12-21 06:04:19', '2026-01-06 21:35:20');
+(1, 'Rapat Tahun Baru', 'Tasyakuran bersama warga dawuan untuk mempererat silaturahm', 'galeri d1.jpg', '2026-01-03 22:30:02', '2026-01-06 15:55:53'),
+(2, 'Musyawarah Desa', 'Penyambung keharmonisan desa kedawung dengan menjaga kesehatan', 'galeri d3.jpg', '2026-01-03 22:30:02', '2026-01-06 21:35:20');
 
 -- --------------------------------------------------------
 
@@ -89,11 +89,11 @@ CREATE TABLE `galeris` (
 --
 
 INSERT INTO `galeris` (`id`, `image`, `created_at`, `updated_at`) VALUES
-(2, 'galeri d1.jpg', '2022-12-21 05:14:06', '2026-01-06 15:51:30'),
-(3, 'Screenshot 2026-01-07 055222.png', '2022-12-21 05:14:25', '2026-01-06 15:52:35'),
-(4, 'galeri d2.jpg', '2022-12-21 05:14:41', '2026-01-06 15:51:40'),
-(5, 'g1-01.jpg', '2022-12-21 05:44:52', '2022-12-21 05:44:52'),
-(6, 'galeri d3.jpg', '2022-12-21 05:45:49', '2026-01-06 15:51:51');
+(1, 'galeri d1.jpg', '2026-01-03 22:30:02', '2026-01-06 15:51:30'),
+(2, 'Screenshot 2026-01-07 055222.png', '2026-01-03 22:30:02', '2026-01-06 15:52:35'),
+(3, 'galeri d2.jpg', '2026-01-03 22:30:02', '2026-01-06 15:51:40'),
+(4, 'g1-01.jpg', '2026-01-03 22:30:02', '2022-12-21 05:44:52'),
+(5, 'galeri d3.jpg', '2026-01-03 22:30:02', '2026-01-06 15:51:51');
 
 -- --------------------------------------------------------
 
@@ -147,8 +147,8 @@ CREATE TABLE `news` (
 --
 
 INSERT INTO `news` (`id`, `title`, `description`, `image`, `created_at`, `updated_at`) VALUES
-(1, 'Bikin Geger Penemuan Mayat', 'KBRN, Majalengka: Keheningan hamparan sawah Desa Balida, Kecamatan Dawuan, Kabupaten Majalengka, Jawa Barat, mendadak pecah oleh kabar duka. Sesosok mayat pria tanpa identitas ditemukan tergeletak tak bernyawa di tengah persawahan, Selasa (30/12/2025) sekitar pukul 11.00 WIB, menggegerkan warga sekitar.', '1767739340_Screenshot 2026-01-07 051522.png', '2022-12-21 05:08:44', '2026-01-06 15:59:59'),
-(2, 'Ramaikan Lomba Catur', 'Ayo dulur dulur gabung lomba catur, sambung silaturahmi bersama warga dawuan', 'magnus.jpg', '2022-12-21 05:50:39', '2026-01-06 15:05:40');
+(1, 'Bikin Geger Penemuan Mayat', 'KBRN, Majalengka: Keheningan hamparan sawah Desa Balida, Kecamatan Dawuan, Kabupaten Majalengka, Jawa Barat, mendadak pecah oleh kabar duka. Sesosok mayat pria tanpa identitas ditemukan tergeletak tak bernyawa di tengah persawahan, Selasa (30/12/2025) sekitar pukul 11.00 WIB, menggegerkan warga sekitar.', '1767739340_Screenshot 2026-01-07 051522.png', '2026-01-03 22:30:02', '2026-01-06 15:59:59'),
+(2, 'Ramaikan Lomba Catur', 'Ayo dulur dulur gabung lomba catur, sambung silaturahmi bersama warga dawuan', 'magnus.jpg', '2026-01-03 22:30:02', '2026-01-06 15:05:40');
 
 -- --------------------------------------------------------
 
@@ -182,10 +182,10 @@ CREATE TABLE `perangkats` (
 --
 
 INSERT INTO `perangkats` (`id`, `title`, `description`, `image`, `created_at`, `updated_at`) VALUES
-(1, 'Kepala Desa', 'Abdul Rohman Baehaqi S.Sy', 'user.jpg.jpg', '2022-12-21 04:11:07', '2026-01-06 15:16:49'),
-(2, 'Wakil Kepala Desa', 'Usep Saepudin', 'user.jpg.jpg', '2022-12-21 04:11:46', '2026-01-06 15:17:48'),
-(3, 'Bendahara', 'Anonim', 'user.jpg.jpg', '2022-12-21 04:13:25', '2026-01-06 21:32:31'),
-(4, 'Sekertaris', 'Anonim', 'user.jpg.jpg', '2022-12-21 04:13:51', '2026-01-06 21:32:45');
+(1, 'Kepala Desa', 'Abdul Rohman Baehaqi S.Sy', 'user.jpg.jpg', '2026-01-03 22:30:02', '2026-01-06 15:16:49'),
+(2, 'Wakil Kepala Desa', 'Usep Saepudin', 'user.jpg.jpg', '2026-01-03 22:30:02', '2026-01-06 15:17:48'),
+(3, 'Bendahara', 'Anonim', 'user.jpg.jpg', '2026-01-03 22:30:02', '2026-01-06 21:32:31'),
+(4, 'Sekertaris', 'Anonim', 'user.jpg.jpg', '2026-01-03 22:30:02', '2026-01-06 21:32:45');
 
 -- --------------------------------------------------------
 
@@ -226,7 +226,7 @@ CREATE TABLE `sambutans` (
 --
 
 INSERT INTO `sambutans` (`id`, `title`, `description`, `image`, `created_at`, `updated_at`) VALUES
-(1, 'Selamat Tahun Baru', 'Assalamualaikum wr.wb \r\nTidak terasa saat ini kita sudah ada di awal tahun 2026. Saya segenap Kepala Desa Dawuan mengucapkan Selamat tahun baru, semoga apa yang kita laksanakan pada tahun ini selalu berkah .', 'user.jpg.jpg', '2022-12-21 10:59:09', '2026-01-06 15:11:50');
+(1, 'Selamat Tahun Baru', 'Assalamualaikum wr.wb \r\nTidak terasa saat ini kita sudah ada di awal tahun 2026. Saya segenap Kepala Desa Dawuan mengucapkan Selamat tahun baru, semoga apa yang kita laksanakan pada tahun ini selalu berkah .', 'user.jpg.jpg', '2026-01-03 22:30:02', '2026-01-06 15:11:50');
 
 -- --------------------------------------------------------
 
@@ -248,12 +248,12 @@ CREATE TABLE `services` (
 --
 
 INSERT INTO `services` (`id`, `title`, `description`, `image`, `created_at`, `updated_at`) VALUES
-(1, 'Pembuatan KTP', 'Layanan pembuatan KTP elektronik bagi warga berusia 17 tahun ke atas, yang hilang, atau rusak. Persyaratan: fotokopi KK, surat pengantar RT/RW, dan pas foto 3x4. Proses 7-14 hari kerja.', 'service.png', '2022-12-21 04:20:22', '2026-01-06 15:46:18'),
-(2, 'Pembuatan Kartu Keluarga', 'Layanan penerbitan atau pembaruan KK untuk warga yang baru menikah, pindah, atau ada perubahan data keluarga. Diperlukan akta nikah/cerai, KTP anggota keluarga, dan surat pengantar RT/RW. Selesai dalam 7-10 hari kerja.', 'service.png', '2022-12-21 04:25:49', '2026-01-06 15:46:29'),
-(3, 'Pembuatan Akte Kelahiran', 'Layanan pengurusan akta kelahiran untuk bayi baru lahir atau anak yang belum memiliki akta. Syarat: surat keterangan lahir dari bidan/RS, fotokopi KTP dan KK orang tua, buku nikah. Proses 14 hari kerja.', 'service.png', '2022-12-21 04:26:21', '2026-01-06 15:46:41'),
-(4, 'Pembuatan AKte Kematian', 'Layanan penerbitan akta kematian sebagai bukti resmi pencatatan sipil. Diperlukan surat keterangan kematian dari RS/dokter, fotokopi KTP dan KK almarhum, serta surat pengantar RT/RW. Selesai dalam 3-7 hari kerja', 'service.png', '2022-12-21 04:33:50', '2026-01-06 15:46:52'),
-(5, 'Prosedur Pindah Domisili', 'Layanan surat keterangan pindah bagi warga yang berpindah tempat tinggal. Syarat: surat pengantar RT/RW, fotokopi KTP dan KK, surat tidak ada tunggakan. Proses 3-5 hari kerja.', 'service.png', '2022-12-21 04:34:28', '2026-01-06 15:47:09'),
-(6, 'Pembuatan Surat Izin Kematian', 'Layanan penerbitan surat keterangan kematian untuk keperluan administrasi pemakaman dan pencatatan sipil. Diperlukan surat keterangan dari rumah sakit atau dokter, fotokopi KTP almarhum, dan surat pengantar RT/RW. Proses 1-3 hari kerja.', 'service.png', '2022-12-21 04:36:44', '2026-01-06 15:47:54');
+(1, 'Pembuatan KTP', 'Layanan pembuatan KTP elektronik bagi warga berusia 17 tahun ke atas, yang hilang, atau rusak. Persyaratan: fotokopi KK, surat pengantar RT/RW, dan pas foto 3x4. Proses 7-14 hari kerja.', 'service.png', '2026-01-03 22:30:02', '2026-01-06 15:46:18'),
+(2, 'Pembuatan Kartu Keluarga', 'Layanan penerbitan atau pembaruan KK untuk warga yang baru menikah, pindah, atau ada perubahan data keluarga. Diperlukan akta nikah/cerai, KTP anggota keluarga, dan surat pengantar RT/RW. Selesai dalam 7-10 hari kerja.', 'service.png', '2026-01-03 22:30:02', '2026-01-06 15:46:29'),
+(3, 'Pembuatan Akte Kelahiran', 'Layanan pengurusan akta kelahiran untuk bayi baru lahir atau anak yang belum memiliki akta. Syarat: surat keterangan lahir dari bidan/RS, fotokopi KTP dan KK orang tua, buku nikah. Proses 14 hari kerja.', 'service.png', '2026-01-03 22:30:02', '2026-01-06 15:46:41'),
+(4, 'Pembuatan AKte Kematian', 'Layanan penerbitan akta kematian sebagai bukti resmi pencatatan sipil. Diperlukan surat keterangan kematian dari RS/dokter, fotokopi KTP dan KK almarhum, serta surat pengantar RT/RW. Selesai dalam 3-7 hari kerja', 'service.png', '2026-01-03 22:30:02', '2026-01-06 15:46:52'),
+(5, 'Prosedur Pindah Domisili', 'Layanan surat keterangan pindah bagi warga yang berpindah tempat tinggal. Syarat: surat pengantar RT/RW, fotokopi KTP dan KK, surat tidak ada tunggakan. Proses 3-5 hari kerja.', 'service.png', '2026-01-03 22:30:02', '2026-01-06 15:47:09'),
+(6, 'Pembuatan Surat Izin Kematian', 'Layanan penerbitan surat keterangan kematian untuk keperluan administrasi pemakaman dan pencatatan sipil. Diperlukan surat keterangan dari rumah sakit atau dokter, fotokopi KTP almarhum, dan surat pengantar RT/RW. Proses 1-3 hari kerja.', 'service.png', '2026-01-03 22:30:02', '2026-01-06 15:47:54');
 
 -- --------------------------------------------------------
 
@@ -275,10 +275,10 @@ CREATE TABLE `sliders` (
 --
 
 INSERT INTO `sliders` (`id`, `title`, `description`, `image`, `created_at`, `updated_at`) VALUES
-(1, 'Pemandangan Alam', 'Gunung dan Sawah', 'slide1-01.jpg', '2022-12-21 04:05:21', '2026-01-06 21:31:02'),
-(2, 'Kegiatan', 'Balap Kuda', 'slide2-01.jpg', '2022-12-21 04:44:07', '2026-01-06 21:31:40'),
-(3, 'Acara', 'Tasyakuran desa dawuan', 'galeri d1.jpg', '2022-12-21 04:59:02', '2026-01-06 21:31:33'),
-(4, 'Lomba', 'Lomba Catur', 'slide4-01.jpg', '2022-12-21 04:59:28', '2026-01-06 21:31:49');
+(1, 'Pemandangan Alam', 'Gunung dan Sawah', 'slide1-01.jpg', '2026-01-03 22:30:02', '2026-01-06 21:31:02'),
+(2, 'Kegiatan', 'Balap Kuda', 'slide2-01.jpg', '2026-01-03 22:30:02', '2026-01-06 21:31:40'),
+(3, 'Acara', 'Tasyakuran desa dawuan', 'galeri d1.jpg', '2026-01-03 22:30:02', '2026-01-06 21:31:33'),
+(4, 'Lomba', 'Lomba Catur', 'slide4-01.jpg', '2026-01-03 22:30:02', '2026-01-06 21:31:49');
 
 -- --------------------------------------------------------
 
@@ -315,7 +315,7 @@ CREATE TABLE `umkms` (
 --
 
 INSERT INTO `umkms` (`id`, `title`, `description`, `image`, `created_at`, `updated_at`) VALUES
-(1, 'UMKM 1', 'UMKM di Desa Dawuan didukung program bantuan modal dari BAZNAS Majalengka. Usaha lokal seperti kuliner dan kerajinan berkembang melalui bazar di tingkat RW, menunjukkan pertumbuhan ekonomi kreatif masyarakat desa.', 'UMKM DAWUAN.jpeg', '2022-12-21 05:00:30', '2026-01-06 15:55:07');
+(1, 'UMKM 1', 'UMKM di Desa Dawuan didukung program bantuan modal dari BAZNAS Majalengka. Usaha lokal seperti kuliner dan kerajinan berkembang melalui bazar di tingkat RW, menunjukkan pertumbuhan ekonomi kreatif masyarakat desa.', 'UMKM DAWUAN.jpeg', '2026-01-06 15:00:02', '2026-01-06 15:55:07');
 
 -- --------------------------------------------------------
 
@@ -340,20 +340,93 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `level`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(6, 'dawuhan', 'superadmin', 'dawuhan@gmail.com', NULL, '$2y$10$mjXY55ieKkBPTQejtt3MZu0eDSno1dczJI2Hz6HHSlVm2IxZM//Re', 'FERTWYQOKUt6xaSKZBAzk5Viwywl0DK4mP8toL76WozMMqElrrenJiZmAJQ6', '2022-12-19 20:15:29', '2026-01-05 02:45:45'),
-(7, 'Nabila Dwi Paramita', 'admin', 'bila@gmail.com', NULL, '$2y$10$/6qXVTv0La96D5LKYrsYdOTzTGsB4upnwx/gi55tZ4AkUe3QhDEpW', '6ix3OFLDUOuAfX6TgfKROHilLIroYOf3oUk5IQjpEmBpgs52zc7ylqjvTiIk', '2022-12-20 07:56:16', '2022-12-20 07:56:16'),
-(8, 'Kurnia Nur Aisyah', 'admin', 'aisyah@gmail.com', NULL, '$2y$10$H0bA4JdNGkNwt4My4psJ.ejP0/qLGEllOwul.bfzEJst/lR11CfYu', 'orDSP8hY2YZL1clY34FVN2SAcwjzGsMIkefB42tKvhUmV8W3XgE3nrQEWsmS', '2022-12-20 07:58:19', '2022-12-20 07:58:19'),
-(9, 'David Kurniawan', 'admin', 'david@gmail.com', NULL, '$2y$10$jfqvZREslxiJt4VUlBsfhe/VIUqJQJNkJT/QUll4I8dD5d9.NLn5m', 'MBBuxDI7QNZlOxfrnzP3gVaDAorshau5uWc4pEkpbVQGXhWCuGZ4OCVLJe9Z', '2022-12-20 07:59:04', '2022-12-20 07:59:04'),
-(11, 'Nico Tambunan', 'admin', 'nico@gmail.com', NULL, '$2y$10$JQvPgGG0ffECVyEf1l17veTeHzsTZTZ6Ded00InExU1dVl8vDlV0u', 'Y8YXZlGziNffWdJl83GKqvt3Rtb41pghqIGm5lX6qteP6O6IS4bPA0kxbWhF', '2022-12-20 08:03:09', '2022-12-20 08:03:09'),
-(12, 'Nallenaya Ilham Putra', 'admin', 'nalle@gmail.com', NULL, '$2y$10$ZIDz.vcWR0FYyzA630UK2Og0Wa7W/O/IcKsXgaWxFAse1wK/k7Yju', 'NcxnOQgZ5nwu3nooTEnoCF0Qhra9I8cayYMy9WztxHMd3KsnSZ7PjuoMv9j1', '2022-12-20 08:03:09', '2026-01-06 07:36:52'),
-(15, 'ingga', 'admin', 'ingga@hahahah.vom', NULL, '$2y$10$K48R7ZvWWj6rjqwxCuHkquljCzE.MHbm0Ve8NpHMD8MnAwNTXtsYm', NULL, '2026-01-04 03:22:13', '2026-01-04 03:22:13'),
-(16, 'adminingga', 'superadmin', 'adminingga@gmail.fuk', NULL, '$2y$10$/Nl4x0/TXLuGZteXpoZ/n.44v9QYwcJqqTciyANMGQPLFUB3Gp3Rq', NULL, '2026-01-04 03:45:38', '2026-01-04 03:45:38'),
-(17, 'Iqbal Maulana', 'admin', 'iqbal@gmail.com', NULL, '$2y$10$AEDek9N09K9Tf3/J6sxhzeSFz8qC3d34gHtUTCWjqV56Hu1AaIdA2', 'ewsFQ1ZlhhYGSbc8Pt1Ap7QHs29nw8pkCkJZgVKoRErAUWl2VpMmvJAWwPke', '2022-12-20 08:03:09', '2026-01-06 07:36:52'),
-(18, 'naya', 'admin', 'naya@gmail.com', NULL, '$2y$10$rhm7sOB8435OfTW8/oV9.ecqkMm7JDAdVS2qgFZ2ze65G94ais.Z6', NULL, '2026-01-06 21:41:37', '2026-01-06 21:41:37');
+(1, 'Nallenaya Ilham Putra', 'admin', 'nalle@gmail.com', NULL, '$2y$10$ZIDz.vcWR0FYyzA630UK2Og0Wa7W/O/IcKsXgaWxFAse1wK/k7Yju', 'NcxnOQgZ5nwu3nooTEnoCF0Qhra9I8cayYMy9WztxHMd3KsnSZ7PjuoMv9j1', '2022-12-20 08:03:09', '2026-01-06 07:36:52'),
+(2, 'ingga', 'admin', 'ingga@hahahah.vom', NULL, '$2y$10$K48R7ZvWWj6rjqwxCuHkquljCzE.MHbm0Ve8NpHMD8MnAwNTXtsYm', NULL, '2026-01-04 03:22:13', '2026-01-04 03:22:13'),
+(3, 'adminingga', 'superadmin', 'adminingga@gmail.fuk', NULL, '$2y$10$/Nl4x0/TXLuGZteXpoZ/n.44v9QYwcJqqTciyANMGQPLFUB3Gp3Rq', NULL, '2026-01-04 03:45:38', '2026-01-04 03:45:38'),
+(4, 'Iqbal Maulana', 'admin', 'iqbal@gmail.com', NULL, '$2y$10$AEDek9N09K9Tf3/J6sxhzeSFz8qC3d34gHtUTCWjqV56Hu1AaIdA2', 'ewsFQ1ZlhhYGSbc8Pt1Ap7QHs29nw8pkCkJZgVKoRErAUWl2VpMmvJAWwPke', '2022-12-20 08:03:09', '2026-01-06 07:36:52'),
+(5, 'naya', 'admin', 'naya@gmail.com', NULL, '$2y$10$rhm7sOB8435OfTW8/oV9.ecqkMm7JDAdVS2qgFZ2ze65G94ais.Z6', NULL, '2026-01-06 21:41:37', '2026-01-06 21:41:37');
 
 --
 -- Indeks untuk tabel yang dibuang
 --
+
+--
+-- Indeks untuk tabel `data`
+--
+ALTER TABLE `data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `galeris`
+--
+ALTER TABLE `galeris`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `perangkats`
+--
+ALTER TABLE `perangkats`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `sambutans`
+--
+ALTER TABLE `sambutans`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `services`
+--
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `sliders`
+--
+ALTER TABLE `sliders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `testimonis`
+--
+ALTER TABLE `testimonis`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `umkms`
+--
+ALTER TABLE `umkms`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `users`
@@ -365,6 +438,84 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
+
+--
+-- AUTO_INCREMENT untuk tabel `data`
+--
+ALTER TABLE `data`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `events`
+--
+ALTER TABLE `events`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `galeris`
+--
+ALTER TABLE `galeris`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT untuk tabel `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `perangkats`
+--
+ALTER TABLE `perangkats`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `sambutans`
+--
+ALTER TABLE `sambutans`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `services`
+--
+ALTER TABLE `services`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `sliders`
+--
+ALTER TABLE `sliders`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `testimonis`
+--
+ALTER TABLE `testimonis`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `umkms`
+--
+ALTER TABLE `umkms`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
